8deaba231cbb3b22ca12481b3028635e507851ff
Sviatoslav Stehnii <81622077+sviatoweb@users.noreply.github.com>
2023-11-03 20:15:57 +0200

    rewrite tests

--- src/test/java/ua/edu/ucu/apps/lab71/Lab71ApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/lab71/Lab71ApplicationTests.java
@@ -16,38 +16,42 @@ import java.util.List;
 @SpringBootTest
 class Lab71ApplicationTests {
 
-	@Test
-	void TestFlowerStore() {
-        FlowerBucket flowerbucket = new FlowerBucket();
-
+    @Test
+    void testDeliveryStrategies() {
         Delivery dhldelivery = new DHLDeliveryStrategy();
         Delivery postdelivery = new PostDeliveryStrategy();
 
-        
-        List<Item> buckets = new ArrayList<Item>();
+        List<Item> buckets = new ArrayList<>();
+        FlowerBucket flowerbucket = new FlowerBucket();
         buckets.add(flowerbucket);
+
         assertEquals(dhldelivery.deliver(buckets), "DHL delivery of 1 items");
         assertEquals(postdelivery.deliver(buckets), "Post delivering of 1 items");
+    }
 
-
+    @Test
+    void testPaymentStrategies() {
         Payment creditcard = new CreditCardPaymentStrategy();
         Payment paypal = new PayPalPaymentStrategy();
 
         assertEquals(creditcard.pay(40), "Paid 40.0 with credit card");
         assertEquals(paypal.pay(40), "Paid 40.0 with paypal");
+    }
 
+    @Test
+    void testOrderProcessing() {
+        FlowerBucket flowerbucket = new FlowerBucket();
+        Delivery dhldelivery = new DHLDeliveryStrategy();
+        Payment creditcard = new CreditCardPaymentStrategy();
+
+        List<Item> buckets = new ArrayList<>();
+        buckets.add(flowerbucket);
 
         Order order = new Order(buckets);
         order.setDeliveryStrategy(dhldelivery);
-        order.setPaymentStrategy(new CreditCardPaymentStrategy());
+        order.setPaymentStrategy(creditcard);
 
         assertEquals(order.processOrder(), "Paid with CreditCard using DHL delivery of 1 items");
-
-
-    
-	}
-
-    
-
-
+    }
 }
+
